df2acc1e29c7dc0408476fdfa6ae6b83803dd295
leohsilva7 <leo.h.silva@aluno.senai.br>
2025-06-27 12:57:54 -0300

    ex1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,1 +1,13 @@
+// Seleciona os elementos do HTML
+const botao = document.getElementById('btn');
+const divClique = document.getElementById('clique');
+
+let contador = 0;
+
+divClique.textContent = `Cliques: ${contador}`;
+
+botao.addEventListener('click', () => {
+    contador++;
+    divClique.textContent = `Cliques: ${contador}`;
+});
 
